c0733cd9d875a54d9ef66ca97e4a05dfac4f0125
whisprer <133140193+whisprer@users.noreply.github.com>
2026-07-05 23:42:40 +0100

    summary

--- riemann_analysis/rrlang/target/debug/deps/rrlang.d
+++ riemann_analysis/rrlang/target/debug/deps/rrlang.d
@@ -1,5 +1,5 @@
-C:\github\5hr-conlang\riemann_analysis\rrlang\target\debug\deps\rrlang.d: crates\rrlang-cli\src\main.rs
+D:\code\5hr-conlang\riemann_analysis\rrlang\target\debug\deps\rrlang.d: crates\rrlang-cli\src\main.rs
 
-C:\github\5hr-conlang\riemann_analysis\rrlang\target\debug\deps\rrlang.exe: crates\rrlang-cli\src\main.rs
+D:\code\5hr-conlang\riemann_analysis\rrlang\target\debug\deps\rrlang.exe: crates\rrlang-cli\src\main.rs
 
 crates\rrlang-cli\src\main.rs:
